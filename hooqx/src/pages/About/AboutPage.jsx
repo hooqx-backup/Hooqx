a951ee041a88f4e1f2c929f42300c75caa9c7f98
Pritam <pkdivision123@gmail.com>
2026-04-21 10:54:25 +0530

    pritam first

--- hooqx/src/pages/About/AboutPage.jsx
+++
@@ -1,8 +0,0 @@
-export default function AboutPage() {
-  return (
-    <main style={{ padding: '100px 24px', textAlign: 'center' }}>
-      <h1>About Hooqx</h1>
-      <p>Coming soon.</p>
-    </main>
-  )
-}
